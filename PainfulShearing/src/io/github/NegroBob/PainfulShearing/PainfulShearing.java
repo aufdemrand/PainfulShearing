0bb702d05ad699b8dbbcbc30fa75a3e28c37c0d7
Jeremy Schroeder <jeremy@ssdbg.com>
2013-08-15 19:14:28 -0400

    Add debug, make Random object more random.

--- PainfulShearing/src/io/github/NegroBob/PainfulShearing/PainfulShearing.java
+++ PainfulShearing/src/io/github/NegroBob/PainfulShearing/PainfulShearing.java
@@ -12,6 +12,11 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class PainfulShearing extends JavaPlugin implements Listener {
 	
+	// Make generator a class field to avoid making a new Random object
+	// every PlayerShearEntityEvent.. this will actually result in a 
+	// more random number, too.
+	Random generator = new Random();
+	
 	public void OnEnable(){		
 		getServer().getPluginManager().registerEvents(this, this);
 		
@@ -23,18 +28,22 @@ public class PainfulShearing extends JavaPlugin implements Listener {
 	
 	@EventHandler
 	public void onPlayerShear(PlayerShearEntityEvent event){
+	
+		// Add some debug to make sure this is firing.
+		getLogger().info(event.getPlayer().getName() + " sheared!");
 		
-	    Player player = event.getPlayer();
-	    org.bukkit.Location spot = player.getLocation();
-	    org.bukkit.entity.Entity sheared = event.getEntity();
-		Random generator = new Random();
-	    int chance = generator.nextInt(10) + 1;
+	    
+	    	Player player = event.getPlayer();
+	    	org.bukkit.Location spot = player.getLocation();
+		 org.bukkit.entity.Entity sheared = event.getEntity();
+	    
+		  int chance = generator.nextInt(10) + 1;
 		
-		if(sheared instanceof Sheep){
+		if (sheared instanceof Sheep) {
 			player.playSound(spot, Sound.GHAST_SCREAM, 1.0f, 1.0f);
 			player.sendMessage("That is a sheep");
 						
-		}else{
+		} else {
 			player.sendMessage("That isn't a sheep");
 			
 		}
